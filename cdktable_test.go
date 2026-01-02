e00e32b4c6e23eb5119ad3e9eec4758c562c2856
Jeff Ellis <jellis777@gmail.com>
2026-01-02 14:31:48 -0600

    Initialize

--- cdktable_test.go
+++ cdktable_test.go
@@ -1,4 +1,4 @@
-package main
+package cdktable_test
 
 // import (
 // 	"testing"
---
+++ main/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"cdktable"
+
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/jsii-runtime-go"
+)
+
+func main() {
+	defer jsii.Close()
+
+	app := awscdk.NewApp(nil)
+
+	cdktable.NewCdktableStack(app, "CdktableStack", &cdktable.CdktableStackProps{
+		StackProps: awscdk.StackProps{
+			Env: env(),
+		},
+	})
+
+	app.Synth(nil)
+}
+
+// env determines the AWS environment (account+region) in which our stack is to
+// be deployed. For more information see: https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+func env() *awscdk.Environment {
+	return nil
+
+}
